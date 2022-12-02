24fe16a04998e9d6f0d060660e023bc141ae3eeb
vlad <96194051+Vlad1994karma@users.noreply.github.com>
2022-12-02 16:00:17 +0300

    add first task

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-console.log("23");
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const lastFilm1 = prompt('Какой последний фильм вы посмотрели?', ''),
+      gradeOfFilm1 = prompt('На сколько оцените?', ''),
+      lastFilm2 = prompt('Какой последний фильм вы посмотрели?', ''),
+      gradeOfFilm2 = prompt('На сколько оцените?', '');
+
+personalMovieDB.movies[lastFilm1] = gradeOfFilm1;
+personalMovieDB.movies[lastFilm2] = gradeOfFilm2;
+
+console.log(personalMovieDB);
